1d79362a4b00d8f00fc8a05869fb8ababa5924f8
aks34 <agarg0721@gmail.com>
2022-07-28 22:46:47 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-veiqt5
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-veiqt5)
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React, { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+
+import App from './App';
+
+ReactDOM.render(<App />, document.getElementById("root"));
